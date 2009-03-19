093d6fdcf5ce41ee3485877a7938631ecbec12a8
Elie Roux <elie.roux@telecom-bretagne.eu>
2009-03-19 20:11:18 +0100

    adding luaextra and the possibility to create and call new callbacks, using this possibility to add new callbacks for managing define_font and open_read_file better

--- luatextra.lua
+++ luatextra.lua
@@ -15,9 +15,9 @@ module("luatextra", package.seeall)
 luatextra.modules = luatextra.modules or {}
 
 luatextra.modules['luatextra'] = {
-    version     = 0.9,
+    version     = 0.91,
     name        = "luatextra",
-    date        = "2009/02/26",
+    date        = "2009/03/19",
     description = "Low level functions for LuaTeX basic usage",
     author      = "Elie Roux",
     copyright   = "Elie Roux, 2009",
@@ -181,6 +181,8 @@ function luatextra.provides_module(mod)
     texio.write_nl('log', format("Lua module: %s %s v%.02f %s\n", mod.name, mod.date, mod.version, mod.description))
 end
 
+luatextra.use_module('luaextra')
+
 function luatextra.kpse_module_loader(mod)
   local script = mod .. ".lua"
   local file = kpse.find_file(script, "texmfscripts")
@@ -216,7 +218,135 @@ function luatextra.catcodetabledef(name, number)
     luatextra.catcodetables[truename] = tonumber(number)
 end
 
+function luatextra.open_read_file(filename)
+    local path = kpse.find_file(filename)
+    local env = {
+      ['filename'] = filename,
+      ['path'] = path,
+    }
+    luamcallbacks.call('pre_read_file', env)
+    path = env.path
+    if not path then
+        return
+    end
+    local f = env.file
+    if not f then
+        f = io.open(path)
+        env.file = f
+    end
+    if not f then
+        return
+    end
+    env.reader = luatextra.reader
+    env.close = luatextra.close
+    return env
+end
+
+function luatextra.reader(env)
+    local line = (env.file):read()
+    line = luamcallbacks.call('file_reader', env, line)
+    return line
+end
+
+function luatextra.close(env)
+    (env.file):close()
+    luamcallbacks.call('file_close', env)
+end
+
+function luatextra.default_reader(env, line)
+    return line
+end
+
+function luatextra.default_close(env)
+    return
+end
+
+function luatextra.default_pre_read(env)
+    return env
+end
+
+function luatextra.find_font(name)
+    local types = {'ofm', 'ovf', 'opentype fonts', 'truetype fonts'}
+    local path = kpse.find_file(name)
+    if path then return path end
+    for _,t in pairs(types) do
+        path = kpse.find_file(name, t)
+        if path then return path end
+    end
+    return nil
+end
+
+function luatextra.font_load_error(error)
+    luatextra.module_warning('luatextra', string.format('%s\nloading lmr10 instead...', error))
+end
+
+function luatextra.load_default_font(size)
+    return font.read_tfm("lmr10", size)
+end
+
+function luatextra.define_font(name, size)
+    if (size < 0) then size = (- 655.36) * size end
+    local fontinfos = {
+        asked_name = name,
+        name = name,
+        size = size
+        }
+    callback.call('font_syntax', fontinfos)
+    local path = fontinfos.path
+    if not path then
+        path = luatextra.find_font(name)
+        fontinfos.path = luatextra.find_font(name)
+    end
+    if not path then
+        luatextra.font_load_error("unable to find font "..name)
+        return luatextra.load_default_font(size)
+    end
+    if not fontinfos.filename then
+        fontinfos.filename = fpath.basename(path)
+    end
+    local ext = fpath.suffix(path)
+    local f
+    if ext == 'tfm' or ext == 'ofm' then
+        f =  font.read_tfm(name, size)
+    elseif ext == 'vf' or ext == 'ovf' then
+        f =  font.read_vf(name, size)
+    elseif ext == 'ttf' or ext == 'otf' or ext == 'ttc' then
+        f = callback.call('open_otf_font', fontinfos)
+    else
+        luatextra.font_load_error("unable to find font "..name)
+        f = luatextra.load_default_font(size)
+    end
+    if not f then
+        luatextra.font_load_error("unable to find font "..name)
+        f = luatextra.load_default_font(size)
+    end
+    callback.call('post_font_opening', f, fontinfos)
+    return f
+end
+
+function luatextra.default_font_syntax(fontinfos)
+    return
+end
+
+function luatextra.default_open_otf(fontinfos)
+    return nil
+end
+
+function luatextra.default_post_font(f, fontinfos)
+    return true
+end
+
 do
+    luatextra.use_module('luamcallbacks')
+    callback.create('pre_read_file', 'simple', luatextra.default_pre_read)
+    callback.create('file_reader', 'data', luatextra.default_reader)
+    callback.create('file_close', 'simple', luatextra.default_close)
+    callback.add('open_read_file', luatextra.open_read_file, 'luatextra.open_read_file')
+    callback.create('font_syntax', 'simple', luatextra.default_font_syntax)
+    callback.create('open_otf_font', 'first', luatextra.default_open_otf)
+    callback.create('post_font_opening', 'simple', luatextra.default_post_font)
+    callback.add('define_font', luatextra.define_font, 'luatextra.define_font')
+
     if luatextrapath then
         texio.write(')')
     end
